2815d95362aff83475ffc0b7931720de07208658
GarrettCrippen <garrettcrippen@gmail.com>
2021-08-03 01:15:32 -0700

    changed variable 'first' to hashSlice

--- import requests.py
+++ import requests.py
@@ -7,7 +7,8 @@ password = sha1(input("input a password: ").encode())
 hashSlice = password[:5]
 key = password[5:-1]
 
+print("Key: ", key,"\n\n")
 #Make an api call for HaveIBeenPwned
-response = requests.request("GET", url+first)
+response = requests.request("GET", url+hashSlice)
 
 print(response.text)
